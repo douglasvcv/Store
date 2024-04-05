d2f02c8ab2ba839c60f839e02e5cb04cabcd52a0
Douglas <douglasvitor1233@gmail.com>
2024-04-04 21:16:01 -0300

    first commit

---
+++ front/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar(){
+
+
+    return (
+        <>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Sobre Nós</a></li>
+            <li><a href="#">Fale conosco</a></li>
+        </ul>
+        
+        </>
+    )
+}
+
+export default Navbar
