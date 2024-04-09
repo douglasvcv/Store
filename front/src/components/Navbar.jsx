0bfee782dabf2702aa1cea4d917a2f6d00284105
Douglas <douglasvitor1233@gmail.com>
2024-04-08 21:55:18 -0300

    add navBar

--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -3,11 +3,25 @@ function Navbar(){
 
     return (
         <>
+        <div className="top_Nav">
+
+            <a href="#" target="blank">Bem-vindo, faça seu login ou cadastre-se</a>
+            <input type="text" name="item" id="item" placeholder="O que procura?"/>
+            <a href="#">Meu carrinho</a>
+        </div>
+        <div className="itens">
         <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Sobre Nós</a></li>
-            <li><a href="#">Fale conosco</a></li>
+            <li><a href="#">Para o celular</a></li>
+            <li><a href="#">Capas</a></li>
+            <li><a href="#">Películas</a></li>
+            <li><a href="">Fones de ouvido</a></li>
+            <li><a href="">Carregadores</a></li>
+            <li><a href="">Cabos</a></li>
+            <li><a href="">Acessórios</a></li>
+            <li><a href="">Mochilas</a></li>
+            <li><a href="">Gamer</a></li>
         </ul>
+        </div>
         
         </>
     )
