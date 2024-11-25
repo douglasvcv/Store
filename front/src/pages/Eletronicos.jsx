298346d7f6e91d641dcd6a4a2350fa594dcce04d
douglasvcv <douglasvitor1233@gmail.com>
2024-11-24 21:13:50 -0300

    testing api another form

--- front/src/pages/Eletronicos.jsx
+++ front/src/pages/Eletronicos.jsx
@@ -1,10 +1,42 @@
-import ApiCategory from "../api/ApiCategory"
+import { useEffect, useState } from "react"
+
 
 function Eletronicos(){
-   
-   
+   const [data, setData] = useState([])
+   const [loading, setLoading] = useState(true)
+   const [erro, setErro] = useState(null)
+
+   useEffect(()=> {
+    const fetchData = async ()=>{
+        try {
+            const response = await fetch(`https://fakestoreapi.com/products/category/men's clothing`)
+            if(!response.ok){
+                setLoading(false)
+                console.log("Erro na requisição")
+                
+            }
+            const data = response.json()
+            setData(data)
+            setLoading(false)
+        } catch (error) {
+            setErro(error)
+        }
+    }
+
+    fetchData()
+   }, [])
+   if(loading) return <p>Carregando...</p>
+   if(erro) return <p>Erro: {erro}</p>
    return <>
-   <ApiCategory category={"men's clothing"} Title={"Eletrônicos"}/>
+    <div>
+        <ul>
+            {data.map(
+                (item) => {
+                    <li>Id:{item.id} </li>
+                }
+            )}
+        </ul>
+    </div>
     </>
 }
 
